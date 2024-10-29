eaaa338c6fc1336cb614d064450ada3fb3ffcdbd
Nitin <bhadoriaabhishek@gmail.com>
2024-10-29 11:41:37 +0530

    Task1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Inter' : ["Inter","sans-serif","Danfo"],
+        'ch' : ["Vina Sans","serif"]
+
+      }
+    },
+  },
+  plugins: [],
+}
+
